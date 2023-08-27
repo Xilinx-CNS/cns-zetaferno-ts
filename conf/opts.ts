c9003d96898083c8f4cecfee552aafedff14828c
Yurij Plotnikov <yurij.plotnikov@arknetworks.am>
2023-08-27 16:14:09 +0100

    conf: configure core dumps logging
    Use ta_core_watcher application to log information about core dumps
    generated by TA, RPC server and other binaries in TA directory, as
    it was done in sapi-ts.
    
    Signed-off-by: Yurij Plotnikov <yurij.plotnikov@arknetworks.am>
    Reviewed-by: Denis Pryazhennikov <denis.pryazhennikov@arknetworks.am>

--- conf/opts.ts
+++ conf/opts.ts
@@ -2,5 +2,6 @@
 # (c) Copyright 2016 - 2022 Xilinx, Inc. All rights reserved.
 --opts=opts/socktest
 --script=scripts/zetaferno-ts
+--script=scripts/def-core-watchers
 --conf-cs=cs/socktest.yml
 --conf-cs=cs/zetaferno-ts.yml
